8ccf5fb625a1f1fe8c1e7c5d7606288b9de6d126
Supriya Satpute <satpute.supriya17@gmail.com>
2025-06-07 02:47:04 +0530

    multiply done

---
+++ MUL/src/com/cdac/Main.java
@@ -0,0 +1,14 @@
+package com.cdac;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 10;
+		int c = a * b;
+		System.out.println(c);
+		
+	}
+
+}
